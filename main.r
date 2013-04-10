0111ecee5357fa9ee9c6b84f35a04a6b9a6292e6
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-04-11 07:06:36 +1200

    Basic code

---
+++ main.r
@@ -0,0 +1,22 @@
+library('rjags')
+
+# Create the JAGS model object
+m = jags.model(file="model.txt")
+
+# Do some burn-in
+update(m, 10000)
+
+# Do the real MCMC
+draw = jags.samples(m, 10000, thin=1, variable.names = c("H0", "blah"))
+
+# Manipulate the output into a list
+results = list()
+for(name in names(draw))
+{
+	temp = as.array(draw[[name]])[,,1]
+	temp = as.array(temp)
+	if(length(dim(temp)) == 2)
+		temp = t(temp)
+	results[[name]] = as.array(temp)
+}
+
